5af7f92e284562cebdd081eaf09fde36e6c07fa4
“340180977” <340180977@qq.com>
2019-07-09 10:53:32 +0800

    second commit

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -33,13 +33,14 @@ public class Const {
 
     public enum ProductStatusEnum {
         ON_SALE(1, "在线");
-        private String value;
-        private int code;
 
         ProductStatusEnum(int code, String value) {
             this.code = code;
             this.value = value;
         }
+
+        private String value;
+        private int code;
 
         public String getValue() {
             return value;
--- src/main/java/com/mmall/service/impl/CartServiceImpl.java
+++ src/main/java/com/mmall/service/impl/CartServiceImpl.java
@@ -3,7 +3,6 @@ package com.mmall.service.impl;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.mmall.common.Const;
-import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.CartMapper;
 import com.mmall.dao.ProductMapper;
